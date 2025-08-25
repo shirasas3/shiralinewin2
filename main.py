9887cbfef4c2fb04a20b4c411065846e1e02e107
jbt <aline.shkedi@gmail.com>
2025-08-25 15:31:29 +0300

    fhfg commiot

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+
 import pygame
 import game_field
-import consts
+import soldier
+import screen
+
 
 state = {
     'up':False,
@@ -13,6 +17,16 @@ state = {
     'touch_mine' : False,
 }
 
+def main():
+    pygame.init()
+    screen.normal_screen()
+    while state['running']:
+        handle_user_events()
+
+
+
+
+
 
 def handle_user_events():
     pygame.init()
@@ -20,37 +34,68 @@ def handle_user_events():
     x = 0
     y = 0
     if keys[pygame.K_UP]:
-        y -= 25
+        soldier.key_up(y)
+        state["up"] = True
     if keys[pygame.K_DOWN]:
-        y += 25
+        soldier.key_down(y)
+        state["down"] = True
     if keys[pygame.K_RIGHT]:
-        x += 25
+        soldier.key_right(x)
+        state["right"] = True
     if keys[pygame.K_LEFT]:
-        x -= 25
+        soldier.key_left(x)
+        state["left"] = True
     if keys[pygame.K_RETURN]:
-        state["change_game"] = True
+        screen.night_screen()
+        time.sleep(1)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             state["running"] = False
-    return (x,y), state["change_game"], state["running"]
+    return
+
+def move_soldier(board):
+    handle_user_events()
+    if state["up"] == True:
+        for i in range(len(board)):
+            for j in range(len(board(i))):
+                if board[i][j] == "soldier":
+                    board[i][j] = 0
+                    board[i + 1][j] = "soldier"
+    if state['down'] == True:
+        for i in range(len(board)):
+            for j in range(len(board(i))):
+                if board[i][j] == "soldier":
+                    board[i][j] = 0
+                    board[i - 1][j] = "soldier"
+    if state['left'] == True:
+        for i in range(len(board)):
+            for j in range(len(board(i))):
+                if board[i][j] == "soldier":
+                    board[i][j] = 0
+                    board[i][j - 1] = "soldier"
+    if state['down'] == True:
+        for i in range(len(board)):
+            for j in range(len(board(i))):
+                if board[i][j] == "soldier":
+                    board[i][j] = 0
+                    board[i][j + 1] = "soldier"
+    return board
 
 
 
-def is_win():
-    for i in range(4):
-        if i <= 2:
-            for j in range(2):
-                if (i,j) in game_field.index_mine(game_field.game_board):
-                    return True
-    return False
 
-def is_false():
-    for i in range(4):
-        if i > 2:
-            for j in range(consts.LEGS_SOLDIER):
-                if (i,j) in game_field.index_flag(game_field.game_board):
-                    return True
-    return False
+
+move_soldier(game_field.game_board)
+
+
+def is_win(index_):
+    return index_ in game_field.index_flag(game_field.game_board)
+
+
+
+def is_lose(index_):
+    return index_ in game_field.index_mine(game_field.game_board)
 
 
 
